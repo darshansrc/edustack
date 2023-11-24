6ddd30c5ce6e31d06c677a1a38f2afc148bafd05
Darshan Gowda <darshansr1618@gmail.com>
2023-11-24 08:53:37 +0530

    Update page.tsx

--- src/app/admin/classes/page.tsx
+++ src/app/admin/classes/page.tsx
@@ -1,58 +1,336 @@
 "use client";
+import {
+  Button,
+  Table,
+  Space,
+  Modal,
+  Form,
+  Input,
+  Select,
+  message,
+  Popconfirm,
+  Skeleton,
+} from "antd";
+import { EditOutlined, PlusOutlined } from "@ant-design/icons";
 import { db } from "@/lib/firebase-config";
-import { collection, getDocs, DocumentData } from "@firebase/firestore";
-import React, { useState, useEffect } from "react";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  setDoc,
+  doc,
+  updateDoc,
+} from "@firebase/firestore";
+import React, { useEffect, useState } from "react";
 
-interface ClassData {
+interface ClassroomData {
   id: string;
-  data: {
-    // Define the structure of your class data here
-    // Example:
-    className: string;
-    classCode: string;
-    // ... other properties
-  };
+  branch: string;
+  classroomSection: string;
+  currentSemester: number;
+  year: number;
+  classroomStatus: string;
 }
 
-const ManageClasses = () => {
-  const [classesData, setClassesData] = useState<ClassData[]>([]);
+const ManageClassrooms = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+  const [classroomData, setClassroomData] = useState<ClassroomData[]>([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [editingClassroom, setEditingClassroom] =
+    useState<ClassroomData | null>(null);
+  const [form] = Form.useForm();
+  const [dataFetched, setDataFetched] = useState(false);
 
-  const fetchClasses = async () => {
+  const showModal = (classroom?: ClassroomData) => {
+    setEditingClassroom(classroom || null);
+    form.setFieldsValue(classroom || {});
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setEditingClassroom(null);
+    form.resetFields();
+    setIsModalVisible(false);
+  };
+
+  const onFinish = async (values: any) => {
     try {
-      const querySnapshot = await getDocs(collection(db, "database"));
-      const fetchedClassesData: ClassData[] = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        data: {
-          className: doc.data().className,
-          classCode: doc.data().classCode,
-          // Add other properties as needed
-        },
-      }));
-      setClassesData(fetchedClassesData);
+      if (editingClassroom) {
+        // Edit existing classroom
+        const classroomDocRef = doc(db, "database", editingClassroom.id);
+
+        // Filter out falsy values
+        const filteredValues: Partial<ClassroomData> = Object.fromEntries(
+          Object.entries(values).filter(
+            ([key, value]) => value !== undefined && value !== null
+          )
+        );
+
+        // Use updateDoc to update only specified fields
+        await updateDoc(classroomDocRef, filteredValues);
+
+        messageApi.success("Classroom updated successfully!");
+
+        setClassroomData((prevClassroomData) =>
+          prevClassroomData.map((classroom) =>
+            classroom.id === editingClassroom.id
+              ? { ...classroom, ...values }
+              : classroom
+          )
+        );
+      } else {
+        // Add new classroom
+        // Generate the ID based on specified conditions
+        const classroomId = values.classroomSection
+          ? `${values.year}-${values.branch}-${values.classroomSection}`
+          : `${values.year}-${values.branch}`;
+
+        // Use setDoc to set a document with the generated ID
+
+        const docRef = doc(db, "database", classroomId);
+        await setDoc(docRef, { ...values, id: classroomId });
+
+        messageApi.success("Classroom added successfully!");
+
+        const newClassroom: ClassroomData = { id: docRef.id, ...values };
+        setClassroomData((prevClassroomData) => [
+          ...prevClassroomData,
+          newClassroom,
+        ]);
+      }
+
+      form.resetFields();
+      setEditingClassroom(null);
+      setIsModalVisible(false);
     } catch (error) {
-      console.error("Error fetching classes:", error);
+      console.error("Error adding/editing classroom:", error);
     }
   };
 
   useEffect(() => {
-    fetchClasses();
-  }, []); // Fetch classes on component mount
+    const fetchClassrooms = async () => {
+      try {
+        setDataFetched(false);
+        const classroomSnapshot = await getDocs(collection(db, "database"));
+        const fetchedClassroomData: ClassroomData[] =
+          classroomSnapshot.docs.map((doc) => ({
+            id: doc.id,
+            branch: doc.data().branch,
+            classroomSection: doc.data().classroomSection,
+            currentSemester: doc.data().currentSemester,
+            year: doc.data().year,
+            classroomStatus: doc.data().classroomStatus,
+          }));
+        setClassroomData(fetchedClassroomData);
+        setDataFetched(true);
+      } catch (error) {
+        messageApi.error("Error fetching classrooms!");
+      }
+    };
+
+    fetchClassrooms();
+  }, []);
+
+  const columns = [
+    {
+      title: "Class ID",
+      dataIndex: "id",
+      key: "id",
+      width: 100,
+    },
+    {
+      title: "Branch",
+      dataIndex: "branch",
+      key: "branch",
+      width: 100,
+    },
+    {
+      title: "Classroom Section",
+      dataIndex: "classroomSection",
+      key: "classroomSection",
+      width: 100,
+    },
+    {
+      title: "Current Semester",
+      dataIndex: "currentSemester",
+      key: "currentSemester",
+      width: 100,
+    },
+    {
+      title: "Year",
+      dataIndex: "year",
+      key: "year",
+      width: 100,
+    },
+    {
+      title: "Classroom Status",
+      dataIndex: "classroomStatus",
+      key: "classroomStatus",
+      width: 100,
+    },
+    {
+      title: "Action",
+      key: "action",
+      width: 100,
+      render: (text: string, record: ClassroomData) => (
+        <Space size="middle">
+          <Button
+            type="primary"
+            icon={<EditOutlined />}
+            onClick={() => showModal(record)}
+          >
+            Edit
+          </Button>
+          <Button
+            type="primary"
+            onClick={() => {
+              /* Redirect to /admin/classes/[ID] */
+            }}
+          >
+            Go to Class
+          </Button>
+        </Space>
+      ),
+    },
+  ];
 
   return (
-    <div>
-      <h1>Manage Classes</h1>
-      {/* Render your classesData here */}
-      {classesData.map((classItem) => (
-        <div key={classItem.id}>
-          {/* Render class details here */}
-          <p>Class id: {classItem.id}</p>
-          <p>Class Name: {classItem.data.className}</p>
-          <p>Class Code: {classItem.data.classCode}</p>
-          {/* Add other property renderings as needed */}
-        </div>
-      ))}
+    <div className="flex flex-col items-left mt-8 pl-4 pr-4 max-w-full">
+      {contextHolder}
+      <div className="mb-4 w-full flex items-center justify-between flex-row">
+        <p className="pl-2 font-semibold text-xl text-gray-700">
+          Manage Classrooms
+        </p>
+        <Button
+          type="primary"
+          icon={<PlusOutlined />}
+          onClick={() => showModal()}
+        >
+          Add New Classroom
+        </Button>
+      </div>
+      <div className="border rounded-lg overflow-x-auto overflow-y-auto">
+        {dataFetched ? (
+          <Table
+            columns={columns}
+            dataSource={classroomData}
+            size="small"
+            className="w-full min-w-full max-w-[calc(100%-200px)]"
+          />
+        ) : (
+          <Skeleton active className="w-full p-10 max-w-[calc(100%-200px)]" />
+        )}
+      </div>
+
+      <Modal
+        title={editingClassroom ? "Edit Classroom" : "Add New Classroom"}
+        open={isModalVisible}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="cancel" onClick={handleCancel}>
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" onClick={form.submit}>
+            {editingClassroom ? "Update" : "Add"}
+          </Button>,
+        ]}
+      >
+        <Form form={form} onFinish={onFinish} layout="vertical">
+          <Form.Item
+            label="Branch"
+            name="branch"
+            rules={[
+              {
+                required: editingClassroom ? false : true,
+                message: "Please select the branch!",
+              },
+            ]}
+          >
+            <Select placeholder="Select branch" disabled={!!editingClassroom}>
+              <Select.Option value="CSE">CSE</Select.Option>
+              <Select.Option value="ISE">ISE</Select.Option>
+              <Select.Option value="ECE">ECE</Select.Option>
+              <Select.Option value="ME">ME</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="Classroom Section"
+            name="classroomSection"
+            // rules={[
+            //   {
+            //     required: editingClassroom ? false : true,
+            //     message: "Please select the classroom section!",
+            //   },
+            // ]}
+          >
+            <Select
+              placeholder="Select classroom section"
+              disabled={!!editingClassroom}
+            >
+              <Select.Option value="">No Section</Select.Option>
+              <Select.Option value="A">A</Select.Option>
+              <Select.Option value="B">B</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="Current Semester"
+            name="currentSemester"
+            rules={[
+              {
+                required: true,
+                message: "Please select the current semester!",
+              },
+            ]}
+          >
+            <Select placeholder="Select current semester">
+              {[1, 2, 3, 4, 5, 6, 7, 8].map((semester) => (
+                <Select.Option key={semester} value={semester}>
+                  {semester}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="Year"
+            name="year"
+            rules={[
+              {
+                required: editingClassroom ? false : true,
+                message: "Please select the year!",
+              },
+            ]}
+            shouldUpdate
+          >
+            <Select
+              placeholder="Select year of scheme"
+              disabled={!!editingClassroom}
+            >
+              {[2020, 2021, 2022, 2023, 2024, 2025].map((year) => (
+                <Select.Option key={year} value={year}>
+                  {year}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="Classroom Status"
+            name="classroomStatus"
+            rules={[
+              {
+                required: true,
+                message: "Please select the classroom status!",
+              },
+            ]}
+          >
+            <Select placeholder="Select classroom status">
+              <Select.Option value="Active">Active</Select.Option>
+              <Select.Option value="Inactive">Inactive</Select.Option>
+            </Select>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 };
 
-export default ManageClasses;
+export default ManageClassrooms;
